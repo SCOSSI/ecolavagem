22897e7e0f483678040de66b57acf9352f87214f
Gabriel <gabriel@gabriel.scossi>
2017-07-11 22:09:16 -0300

    Initial commit

---
+++ src/main/java/com/ecolavagem/ecolavagem/EcolavagemApplication.java
@@ -0,0 +1,12 @@
+package com.ecolavagem.ecolavagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcolavagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcolavagemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ecolavagem/ecolavagem/EcolavagemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ecolavagem.ecolavagem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EcolavagemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
