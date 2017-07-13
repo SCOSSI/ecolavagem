6ffa4f5c25110bf66867b9974beea47c79e6b5f4
Alexandre S <alexandre.strevenski@gmail.com>
2017-07-13 13:34:53 -0300

    spring-data-jpa dependency JpaRepository included

---
+++ src/main/java/com/ecolavagem/ecolavagem/model/entity/CarWasher.java
@@ -0,0 +1,7 @@
+package com.ecolavagem.ecolavagem.model.entity;
+
+/**
+ * Created by Alexandre S on 13/07/2017.
+ */
+public class CarWasher {
+}
---
+++ src/main/java/com/ecolavagem/ecolavagem/repository/CarWasherRepository.java
@@ -0,0 +1,8 @@
+package com.ecolavagem.ecolavagem.repository;
+
+import com.ecolavagem.ecolavagem.model.entity.CarWasher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarWasherRepository extends JpaRepository<CarWasher, Long> {
+
+}
